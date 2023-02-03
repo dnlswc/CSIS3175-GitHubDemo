b33a6d271ac89e8df2fa8ef4bbb9eb932787a976
Daniel Choi <45144687+dnlswc@users.noreply.github.com>
2023-02-03 04:45:17 -0800

    Second commit from local machine

--- app/src/main/java/com/example/csis3175_githubdemo/MainActivity.java
+++ app/src/main/java/com/example/csis3175_githubdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 // added a comment from central repo
 public class MainActivity extends AppCompatActivity {
 
+    // made another change locally from lab computer
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
